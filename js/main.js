4662c0caa04168066486729152982dc465a60834
Ezeiru-Elias Chukwubuikem Emma <105207318+Ezeiruemma@users.noreply.github.com>
2023-06-20 17:26:00 +0100

    fixed sign up form added back function

--- js/main.js
+++ js/main.js
@@ -2,6 +2,10 @@ const btn = document.querySelector(".btn-rounded");
 const showcase = document.querySelector(".showcase-content");
 const log = document.querySelector(".log");
 const back = document.querySelector('.back')
+const btnxl = document.querySelector(".btn-xl")
+const signup = document.querySelector(".signup")
+const bac = document.querySelector(".bac")
+
 
 btn.addEventListener("click",function(){
     showcase.classList.add("hide")
@@ -11,4 +15,14 @@ btn.addEventListener("click",function(){
 back.addEventListener('click',function(){
     showcase.classList.remove("hide");
     log.classList.remove("show");
+})
+
+btnxl.addEventListener("click",function(){
+    showcase.classList.add("hid")
+    signup.classList.add("sho")
+})
+
+bac.addEventListener("click",function(){
+    showcase.classList.remove("hid")
+    signup.classList.remove("sho")
 })
