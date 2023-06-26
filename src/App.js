5c75da6074862287e195294a6b2e50788751dd6b
Ezeiru-Elias Chukwubuikem Emma <105207318+Ezeiruemma@users.noreply.github.com>
2023-06-26 13:08:57 +0100

    working on watchmind mainpage

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Row from './Row';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hey my name is emma</h1>
+      <Row title= "NETFLIX ORIGINALS" />
+      <Row title="Trending Now" />
+    </div>
+  );
+}
+
+export default App;
